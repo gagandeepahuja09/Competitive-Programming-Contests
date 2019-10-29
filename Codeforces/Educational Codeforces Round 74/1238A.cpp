4076738b099001312320d9f3b668d852fd4e84b0
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-10-29 13:00:18 +0530

    very easy

---
+++ Codeforces/Educational Codeforces Round 74/1238A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+int t;
+cin >> t;
+while(t--) {
+    ll n, r;
+    cin >> n >> r;
+    ll a[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a, a + n);
+    n = unique(a, a + n) - a;
+    ll ans = 0;
+    for(int i = n - 1; i >= 0; i--) {
+        if(a[i] - ans * r > 0) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+}
