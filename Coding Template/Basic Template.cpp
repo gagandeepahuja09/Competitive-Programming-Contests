16815c4fccc26c90780e02384b6d3bed616d311e
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-05-01 11:09:18 +0530

    Create Basic Template.cpp

---
+++ Coding Template/Basic Template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define vi vector<int>
+#define vii vector<vi>
+#define read(a)  for(int i = 0; i < n; i++) cin >> a[i];
+#define print(a)  for(int i = 0; i < n; i++) cout << a[i] << " ";
+
+signed main() {
+    int t = 1;
+    // cin >> t
+    while(t--) {
+ 
+    }
+}
