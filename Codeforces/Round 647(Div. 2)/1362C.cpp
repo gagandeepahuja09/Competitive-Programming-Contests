b76c06be62d4531443f978525210a723cc36ea7f
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-06-05 21:26:56 +0530

    easy

---
+++ Codeforces/Round 647(Div. 2)/1362C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vi>
+#define read(a)  for(int i = 0; i < n; i++) cin >> a[i];
+#define print(a)  for(int i = 0; i < n; i++) cout << a[i] << " ";
+#define pb push_back
+#define pql priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define pqlv priority_queue<vi>
+#define pqsv priority_queue<vi, vvi, greater<vi>>
+#define endl '\n'
+#define N 1234567
+#define MOD 1000000007
+
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(0); 
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int sum = 1, ans = 0, prod = 1;
+        for(int i = 0; i < 64; i++) {
+            int x = (n & (1ll << i));
+            // cout << x << " ";
+            if(x) {
+                ans += sum;
+            }
+            prod *= 2;
+            sum += prod;
+        }
+        cout << ans << endl;
+    }
+	return 0;
+}
