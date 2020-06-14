9b6e627d2d9aa55798ae0e97610fac89e60c49b9
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-06-14 19:17:36 +0530

    very easy

---
+++ Codeforces/Educational Codeforces Round 68/1194A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vi>
+#define read(a)  for(int i = 0; i < n; i++) cin >> a[i];
+#define print(a)  for(int i = 0; i < n; i++) cout << a[i] << " ";
+#define pb push_back
+#define pql priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define pqlv priority_queue<vi>
+#define pqsv priority_queue<vi, vvi, greater<vi>>
+// #define endl '\n'
+#define N 10000002
+#define MOD 1000000007
+
+signed main() {
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int n, x;
+        cin >> n >> x;
+        cout << 2 * x << endl;
+    }
+}
