7cfd13401e2448a00b997e30d028d986e0e85d52
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-06-20 23:25:59 +0530

    very easy

---
+++ AtCoder/ABC 148/B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define vi vector<int>
+#define pb push_back
+#define vvi vector<vi>
+
+signed main() {
+    int n;
+    cin >> n;
+    string s, t;
+    cin >> s >> t;
+    for(int i = 0; i < s.size(); i++) {
+        cout << s[i] << t[i];
+    }
+}
